c99cc7192a5a5f3833dde0589bd2b941305a731f
nakrufsevim <furkansevim642@gmail.com>
2025-06-08 00:00:10 +0200

    Database Implementation

--- models.py
+++ models.py
@@ -53,15 +53,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)  # Added is_active attribute
 
     def __init__(self, username, password):
         self.username = username
-        # Hash the password before storing it
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
-        """Check if the provided password matches the hashed password."""
         return bcrypt.check_password_hash(self.password, password)
+
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
